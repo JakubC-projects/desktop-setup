ab2e248bba9ef6e40bad7d90c409c83643aaaf81
jakub czyz <jakub12134@gmail.com>
2022-08-11 22:47:51 +0200

    chmod of ssh key

--- setup_secrets.sh
+++ setup_secrets.sh
@@ -5,5 +5,6 @@ gpg --import-options restore --import keys/gpg-general-new.private.key
 
 echo "Setup ssh key"
 gpg -d keys/ssh-general.key.gpg > ~/.ssh/ssh-general.key
+chmod 600 ~/.ssh/ssh-general.key
 cp ./keys/ssh-general.key.pub ~/.ssh/
 ssh-add ~/.ssh/ssh-general.key
