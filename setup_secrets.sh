fca1f4d2c4b9ad9c8728d15751ce36440f274e60
Jakub Czyz <jakub12134@gmail.com>
2022-08-11 23:19:25 +0200

    update secrets setup file

--- setup_secrets.sh
+++ setup_secrets.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Setup gpg key"
 gpg --import-options restore --import keys/gpg-general-new.private.key
-# mkdir ~/.ssh
+
 
 echo "Setup ssh key"
+mkdir ~/.ssh
 gpg -d keys/ssh-general.key.gpg > ~/.ssh/ssh-general.key
 chmod 600 ~/.ssh/ssh-general.key
 cp ./keys/ssh-general.key.pub ~/.ssh/
