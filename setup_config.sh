a2096f087997d0e4bfa1a4b6aa2eb87f7bd0770c
Jakub Czyz <jakub12134@gmail.com>
2022-08-12 22:38:30 +0200

    update config for gtk

--- setup_config.sh
+++ setup_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -s ~/repos/desktop-setup/config/i3 ~/.config 
 ln -s ~/repos/desktop-setup/config/i3status ~/.config 
+ln -s ~/repos/desktop-setup/config/gtk-3.0 ~/.config 
 
 sudo cp ~/repos/desktop-setup/config/lightdm-gtk-greeter.conf /etc/lightdm/
