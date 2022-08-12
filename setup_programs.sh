98c8c1024a957a35a944f480f3e2affe45c63823
Jakub Czyz <jakub12134@gmail.com>
2022-08-12 22:13:57 +0200

    add zsh

--- setup_programs.sh
+++ setup_programs.sh
@@ -4,7 +4,10 @@ systemctl enable bluetooth
 # Virtual box
 sudo modprobe vboxdrv
 
-# Oh My Zsh
+# ZSH
+$ chsh -s /bin/zsh
+git config --global user.email "jakub12134@gmail.com"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # git
