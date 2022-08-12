227841fc76949c5f9ff57484c9022258ed618128
Jakub Czyz <jakub12134@gmail.com>
2022-08-12 22:21:23 +0200

    update zsh

--- setup_programs.sh
+++ setup_programs.sh
@@ -4,12 +4,10 @@ systemctl enable bluetooth
 # Virtual box
 sudo modprobe vboxdrv
 
-# ZSH
-$ chsh -s /bin/zsh
-git config --global user.email "jakub12134@gmail.com"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # git
 git config --global user.name "Jakub Czyz"
 git config --global user.email "jakub12134@gmail.com"
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
